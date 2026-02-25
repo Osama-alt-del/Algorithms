165acb63e1b794c9a5d9d3cbd3c32f9c594fdaff
Osama-alt-del <abdalghanyosama@gmail.com>
2026-02-25 19:29:08 +0900

    quickSort

--- 1_sorts/test.c
+++ 1_sorts/test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "./1_bubbleSort.h"
 #include "./2_quickSort.h"
-#define SIZE 4 
+#define SIZE 6 
 
 int main(){
 
 	// Can't use variable to define array length?
 	// const size = 3;
 	// int array[SIZE] = {4, 3, 2, 1};
-        int array[SIZE] = {3, 2, 1, 4};
+        int array[SIZE] = {3, 2, 6, 4, 0, -1};
             
 
 	/* Bubble Sort Test */
@@ -16,13 +16,15 @@ int main(){
 	//betterBubbleSort(array, SIZE);
 	
 	/* Quick Sort Test */
-	// quickSort(array, 0, SIZE);
-        partition(array, 0, SIZE);
-
+	quickSort(array, 0, SIZE);
+        // partition(array, 0, SIZE);
+        
+        printf("//-----Array-----//\n");
 	// Check the sorted array
 	for (int i = 0; i < SIZE; i++){
-		printf("%d\n", array[i]);
+		printf("%d ", array[i]);
 	}
+        printf("\n");
 
 	return 0;
 }
