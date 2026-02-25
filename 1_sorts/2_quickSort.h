3046b1d2ee834647cd29ab0626cb579a77e473e1
Osama-alt-del <abdalghanyosama@gmail.com>
2026-02-25 17:51:25 +0900

    Debugging Quicksort

--- 1_sorts/2_quickSort.h
+++ 1_sorts/2_quickSort.h
@@ -1,5 +1,5 @@
 /* このファイルでクイックソートの関数を造ります*/
-
+#include <stdio.h>
 /* What is quick sort?:
 	１．ある適当な値を決めて(最初の値でもいい）、それより大きいものは後ろへ、小さいものは前へ移動する
 	２．二つに分けたそれぞれのグループの中でまた適当な値を決めて、それより大きい
@@ -14,27 +14,23 @@
 // いつ始まる、または、いつ終わるというパラメターが必要
 int partition(int* array, int low, int high){
 	int i, j, temp;
+        i = low;
+        j = high; 
 	int pivot = array[low];
-	while (i < j){
-		do{	/* while loop でも良い？ */
-			i++; /* pivot より大きいのを見つけたらやめて、ｊが動き始める*/
-		}while(array[i] <= pivot); /* ここはなぜ　＞＝　の方が良いのだろう */
-		do{
-			j--; /* pivot より小さいのがあったら、そのindex がわかる*/
-		}while(array[i] > pivot);
-
-		if (i < j){ /* この条件は必要、j とiがwhile のなかでかわるかもしれないから*/
-		// pivot より大きい値と大きい値の位置を入れ替える
+	do{
+		do{i++;}while(array[i] <= pivot);
+		do{j--;}while(array[j] > pivot);
+		if (i < j){
+                        printf("ran");
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
 		}
-	}
-	// pivot のあるべき位置が分かった（ｊ）。だから入れ替える
+	}while(i < j);
 	temp = array[j];
 	array[low] = array[j];
 	array[j] = array[low];
-	return j; /* その位置をリターンする*/
+	return j;
 }
 
 void quickSort(int* array, int low, int high){
--- 1_sorts/test.c
+++ 1_sorts/test.c
@@ -2,20 +2,22 @@
 #include "./1_bubbleSort.h"
 #include "./2_quickSort.h"
 #define SIZE 4 
-void bubbleSort(int*, int);
 
 int main(){
 
 	// Can't use variable to define array length?
 	// const size = 3;
-	int array[SIZE] = {4, 3, 2, 1};
+	// int array[SIZE] = {4, 3, 2, 1};
+        int array[SIZE] = {3, 2, 1, 4};
+            
 
 	/* Bubble Sort Test */
 
 	//betterBubbleSort(array, SIZE);
 	
 	/* Quick Sort Test */
-	quickSort(array, 0, SIZE);
+	// quickSort(array, 0, SIZE);
+        partition(array, 0, SIZE);
 
 	// Check the sorted array
 	for (int i = 0; i < SIZE; i++){
