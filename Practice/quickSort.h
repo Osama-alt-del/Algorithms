a822bd2d958f128c0ed2c918e00f4aeb30378293
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-03 16:24:29 +0900

    Made quickSort with while loops
     - I was playing around with quickSort and tried to make it with while loops instead of do while loops
     - Found out that if I set j to be high-1 then there isn't a segmentation fault (with a do while, j is subtracted before the condition is checked for, so there is no need to subtract beforehand).

--- Practice/quickSort.h
+++ Practice/quickSort.h
@@ -4,16 +4,24 @@ int partition(int* array, int low, int high){
     int i, j, temp, pivot;
     pivot = array[low];
     i = low;
-    j = high;
-    do{// Do while is needed, look this up
-            do{i++;}while(array[i] <= pivot);
-            do{j--;}while(array[j] > pivot);
+    j = high-1;
+    while(i < j){// It's fine for this to be a while loop
+            // If these aren't do while, then there's a seg fault
+            // This problem can be fixed, by simply setting j to be high -1
+            while (array[i] <= pivot){
+                i++;
+            }
+            while (array[j] > pivot){
+                j--;
+            }
+            // do{i++;}while(array[i] <= pivot);
+            // do{j--;}while(array[j] > pivot);
             if (i < j){ // This is necessary
                     temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
             }
-    }while(i < j);
+    }
     // If i<j, then j is pivot's position:
     temp = array[j];
     array[j] = array[low];
