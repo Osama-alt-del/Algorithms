ba3d64d00640dd27054e243e7662825ae0bac6fa
Osama-alt-del <abdalghanyosama@gmail.com>
2026-03-03 22:32:12 +0900

    wip

---
+++ Practice/timer.h
@@ -0,0 +1,10 @@
+// This is a function that times how long functions takes to run in C
+#include <time.h>
+#include <stdlib>
+
+// Make this function not need the exact number of parameters for a function
+// Need to make use of Vardiac function call
+int (void (*function), int, int){
+    int time;
+    return 0;
+}
